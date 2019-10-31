e6019407b72bdf659fa311af20a6bee6258f720b
DavisStudio <51412209+DavisStudio@users.noreply.github.com>
2019-10-31 16:52:17 +0000

    Progress System added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 canvas = document.getElementById("game");
 ctx = canvas.getContext("2d");
 ctx.imageSmoothingEnabled = false;
+ctx.font = "20px Arial";
 
 // PlayerRectangle = 0
 // FishImage = 1
@@ -11,10 +12,13 @@ var drag = 0.99;
 var gravity = 0.22;
 var bounce = 0.55;
 
-var fishMoveSpeed = 25;
-var fishOffsetBounce = 350;
+var fishMoveSpeed = 50;
+var fishOffsetBounce = 300;
 
-var fishingRodWidth = 250;
+var fishingRodWidth = 200;
+
+var progress = 25;
+var inGame = true;
 
 var PLAYER = "PLAYER";
 var FISH = "FISH";
@@ -23,7 +27,7 @@ var minLoad = 1, loadCount = 0;
 
 canvas.addEventListener("click", clickOnCanvas);
 
-makeObject(PLAYER, 100, 200, 100, 300, "green");
+makeObject(PLAYER, 100, 200, 100, fishingRodWidth, "green");
 makeObject(FISH, 100, 200, 100, 70, "blue");
 
 renderList[1].image = new Image();
@@ -33,13 +37,12 @@ renderList[1].image.src = "images/fish.png";
 function render()
 {
     ctx.clearRect(0, 0, 1600, 900);
-
+    
     for(var i = 0; i < renderList.length; i++)
     {
-
     obj = renderList[i];
 
-    //move 
+    //move -------------------------
     if (obj.ID == PLAYER) 
     {
         obj.y -= obj.force;
@@ -47,20 +50,20 @@ function render()
         obj.force -= gravity;
     }
 
-    //collide 
+    //collide ------------------------------
     if(obj.ID == PLAYER)
-    {
+    {  
         if(obj.y < 0)
         {
             obj.force -= obj.force;
             obj.y += 4;
         }
-        else if(obj.y + 300 > 900)
+        else if(obj.y + fishingRodWidth > 900)
         {
-            if(obj.force < 1.5 && obj.force > -1.5)
+            if(obj.force < 1.3 && obj.force > -1.3)
             {
                 obj.force = 0;
-                obj.y = 600
+                obj.y = 900 - fishingRodWidth
             }
             else
             {
@@ -69,20 +72,9 @@ function render()
                 obj.y -= 1;
             }
         }
-        
-        console.log(obj.force);
     }
 
-    if(obj.ID == FISH)
-    {
-        if(obj.pointToMove == null)
-        {
-            obj.getNewPoint();
-        }
-
-        obj.moveToPoint();
-    }
-
+    progressUpdate();
     //draw
     if(obj.ID == FISH)
     {
@@ -154,9 +146,8 @@ function makeObject(ID, x, y, width, height, color)
 
         obj.isPointReached = function()
         {
-            if(this.y > this.pointToMove - 3 && this.y < this.pointToMove + 3)
+            if(this.y > this.pointToMove - 6 && this.y < this.pointToMove + 6)
             {
-                console.log("middle");
                 this.getNewPoint();
             }
         }
@@ -195,4 +186,54 @@ function loadHandler(e) {
 function getRandomNum(min, max)
 {
     return min + Math.random() * (max - min);
+}
+
+function progressUpdate()
+{
+    fish = renderList[1];
+    player = renderList[0];
+
+    if(player.y < fish.y && player.y + fishingRodWidth > fish.y)
+    {
+        progress += 0.2;    
+    }
+
+    if (inGame) 
+    {
+        progress -= 0.1;
+
+        if (fish.pointToMove == null) 
+        {
+            fish.getNewPoint();
+        }
+        else
+        {
+            fish.moveToPoint();
+        }
+        
+        if(progress > 100)
+        {
+            inGame = false;
+        }
+    }
+
+    if(progress < 0)
+    {
+        inGame = false;
+        progress = 0;
+    }
+    else if(progress > 100)
+    {
+        progress = 100;
+        inGame = false;
+    }
+
+    ctx.fillText(Math.floor(progress), 300, 50);
+    
+    ctx.beginPath()
+    ctx.lineWidth = 2;
+    ctx.rect(300,50, 500, 50);
+    ctx.stroke();
+
+    ctx.fillRect(300, 50,  500 * progress / 100, 50);
 }
